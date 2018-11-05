9d176da3531c368850ae075206cf4ace97381702
war-turtle <kartikyadav71199@gmail.com>
2018-11-05 15:08:21 +0530

    index page prototype

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
     $(".sidenav").sidenav();
     $(".parallax").parallax();
-
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 10) {
-            $("nav").css("backgroundColor", "white");
-            $(".change-color").css("color", "black");
-        } else {
-            $("nav").css("backgroundColor", "transparent");
-            $(".change-color").css("color", "white");
-        }
-    });
 });
